bc7d41bc7ab45c7d365b017fdeea21922af4a941
Jack Allen <55121080+fifthawk@users.noreply.github.com>
2021-07-25 16:01:32 -0500

    first commit

--- exercises/SpaceLocation.ts
+++ exercises/SpaceLocation.ts
@@ -1,1 +1,14 @@
 // Paste in the provided code here:
+// Paste in the provided code here:
+export class SpaceLocation {
+    kilometersAway: number;
+    name: string;
+ 
+    constructor(name: string, kilometersAway: number) {
+       this.name = name;
+       this.kilometersAway = kilometersAway;
+    }
+
+
+    }
+ 
